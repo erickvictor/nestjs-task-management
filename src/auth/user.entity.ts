a2d8b2c489a99257c3342323d8ff6373503820cb
Erick Bernardo <erickvictor@gmail.com>
2021-04-14 10:35:56 -0400

    Validating user password for sign in

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -5,6 +5,7 @@ import {
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 @Entity()
 @Unique(['username'])
 export class User extends BaseEntity {
@@ -19,4 +20,9 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
